1e843515d2ba6268bd07a97930257407965944ca
giadainnocenti <giadainnocenti@users.noreply.github.com>
2021-01-08 21:58:54 -0500

    PyPoll solution was added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,13 @@
 import os
 import csv
 
+#function to skip start from a new line
+def text_writer(filename, strin):
+    print(strin)
+    analysis.write(strin)
+    filename.write('\n')
+    return()
+
 # Select the folder containing the CSV file
 csvpath = os.path.join('.','Resources', 'budget_data.csv')
 
@@ -26,31 +33,37 @@ financialanalysis = os.path.join(".", "Output", "financial_analysis.txt")
 #opening the output file
 analysis = open(financialanalysis, 'w') 
 #print the headings on the terminal screen and the output file
-print("```text \n Financial Analysis \n ----------------------------")
-analysis.write("```text \n Financial Analysis \n ---------------------------- \n")
+string = "***Text \n----------------------------\nFinancial Analysis \n----------------------------"
+text_writer(analysis, string)
 
 # Calculate the total number of months included in the dataset
 months = len(period)
-print(f"Total months: {months}")
-analysis.write(f"Total months: {months} \n")
+string = f"Total months: {months}"
+text_writer(analysis, string)
+
 # Evaluate the net total amount of "Profit/Losses" over the entire period
 net_total =  sum(budget_data)
-print(f"Total: ${net_total}")
-analysis.write(f"Total: ${net_total} \n")
+string=f"Total: ${net_total}"
+text_writer(analysis, string)
+
 # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 change = []
 for index in range(len(budget_data)-1):
     change.append(budget_data[index+1]-budget_data[index])
 average_change = sum(change)/len(change)
-print(f"Average  Change: ${average_change:8.2f}")
-analysis.write(f"Average  Change: ${average_change:8.2f} \n")
+string = f"Average  Change: ${average_change:8.2f}"
+text_writer(analysis, string)
+
 # Evaluate the greatest increase in profits (date and amount) over the entire period
 greatest_profit = max(budget_data)
-print(f"Greatest Increase in Profits: {period[budget_data.index(greatest_profit)]} (${greatest_profit})")
-analysis.write(f"Greatest Increase in Profits: {period[budget_data.index(greatest_profit)]} (${greatest_profit}) \n")
+string = f"Greatest Increase in Profits: {period[budget_data.index(greatest_profit)]} (${greatest_profit})"
+text_writer(analysis, string)
+
 # Evaluate the greatest decrease in losses (date and amount) over the entire period
 lowest_profit = min(budget_data)
-print(f"Greatest Decrease in Profits: {period[budget_data.index(lowest_profit)]} (${lowest_profit})")
-analysis.write(f"Greatest Decrease in Profits: {period[budget_data.index(lowest_profit)]} (${lowest_profit}) \n")
+string = f"Greatest Decrease in Profits: {period[budget_data.index(lowest_profit)]} (${lowest_profit})"
+text_writer(analysis, string)
+string = "***"
+text_writer(analysis, string)
 #close the output file
 analysis.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,71 @@
 import os
 import csv
+
+#defining a function to print the delimiter string
+def delimiter(filename, string):
+    print(string)
+    filename.write(string)
+    filename.write('\n')
+    return()
+def divider():
+    return ("-------------------------")
+
+csvpath = os.path.join('.','Resources','election_data.csv')
+
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    voter_ID = []
+    county = []
+    candidates = []
+    for row in csvreader:
+        voter_ID.append(row[0])
+        county.append(row[1])
+        candidates.append(row[2])
+csvfile.close()
+
+resultpath = os.path.join('.','Output','Election_results.txt')
+
+result = open(resultpath, 'w')
+string ="*** Text \nElection Results "
+delimiter(result, string)
+delimiter(result, divider())
+
+
+#The total number of votes cast
+total_votes = len(candidates)
+string = f"Total Votes: {total_votes}"
+delimiter(result, string)
+delimiter(result, divider())
+# A complete list of candidates who received votes
+candidate_list = []
+for candidate in candidates:
+    if candidate not in candidate_list:
+        candidate_list.append(candidate)
+
+winner_votes = 0
+for candidate in candidate_list:
+# The total number of votes each candidate won
+    candidate_votes = candidates.count(candidate)
+# The percentage of votes each candidate won
+    percentage = candidate_votes/total_votes
+    string = f"{candidate}: {percentage:0.3%} ({candidate_votes})"
+    delimiter(result, string)
+    
+# The winner of the election based on popular vote.
+    if candidate_votes > winner_votes:
+        winner_votes = candidate_votes
+        winner = candidate
+    elif candidate_votes == winner_votes:
+        string = f'WARNING: {winner} and {candidate} have the same number of votes'
+        delimiter(result, string)
+
+delimiter(result, divider())
+string = f"Winner: {winner}"
+delimiter(result, string)
+delimiter(result, divider())
+string = f"***"
+delimiter(result, string)
+#close the output file
+result.close()
+
